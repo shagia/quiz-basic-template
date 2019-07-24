92df618cc834b5ffe39cfd751a855e369b665cc7
shagia <mrkraiola@gmail.com>
2019-07-24 13:53:50 -0700

    make containers for the question page and response page

--- js/app.js
+++ js/app.js
@@ -2,20 +2,24 @@
 But if so, I'm writing comments regarding what it's doing to the dataSheet to make a quiz!
 Knowing how loops work'll come in handy.*/
 
-//init variables
-let ansRight = 0
-const coreApp = document.getElementById('app')
-const submit = document.getElementById('submitA')
+// connect to data-old.json
 const data = dataBase
+// bind to DOM elements
+const coreApp = document.getElementById('app')
+const questionContainer = coreApp.childNodes[1]
+const submit = document.getElementById('submitA')
+const continueQ = document.getElementById('continue')
+//convert objects
 const dataEntries = Object.entries(data)
 const dataLength = Object.keys(data).length
-// These two constraints gets the DOM elements of where the responses are rendered, and where the question is rendered.
+// These constraints gets the DOM elements of where the responses are rendered, and where the question is rendered.
 // '7' is where the responses are, '5' is where the question is.
-const quizBox = coreApp.childNodes[7]
-const quizQuestion = coreApp.childNodes[5]
+const quizBox = questionContainer.childNodes[7]
+const quizQuestion = questionContainer.childNodes[5]
 const quizImage = document.getElementById('questionImg')
+// init variables
 let currentPage = 0
-
+let ansRight = 0
 
 /* Checks the currentPage variable in comparison to dataLength => loops through currentPage's objects => passes objects to the makeResponse function and binds the submit button to getRadioAnswer => Renders title, radios and text for each respective array */
 
@@ -24,11 +28,8 @@ function init() {
     if (currentPage != dataLength) {
 
         for (let j = 0; j < dataEntries[currentPage][1].responses.length; j++) {
-            //console.log(dataEntries[currentPage][1].responses[j].response);
-
             makeResponse([j], dataEntries[currentPage][1].responses[j].response, dataEntries[currentPage][1].question)
             quizImage.src = dataEntries[currentPage][1].imageURL
-
         }
         submit.addEventListener("click", function() {
             getRadioAnswer();
@@ -74,17 +75,17 @@ function getRadioAnswer() {
         // If it finds a checked radio box, it should find if it's true in the database
         if (radios[r].checked) {
             console.log("Radio is checked....")
+            
             if (radios[r].value == dataEntries[currentPage][1].answer) {
                 console.log("..and the answer is true!")
-
+                
+                ansRight++
                 // Now there needs to be a confirm screen before we erase and render
-
                 while (quizBox.firstChild) {
 
                     // Kinda lazy, am I properly disposing of the DOM elements (i.e removing it from memory) or would it be better to get more specific?
                     quizBox.removeChild(quizBox.firstChild);
                 }
-
                 // Maybe currentPage should be a variable in init, and init should be renamed, so an actual init could be the point where everything is set up or reset
                 currentPage++
                 init()
